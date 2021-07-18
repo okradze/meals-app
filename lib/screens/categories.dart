99a3d594e87bcff6a6e51e819968335e9036dadd
Mirian <okradzemirian@gmail.com>
2021-07-18 19:20:32 +0400

    feat: scaffold in categories screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:meals_app/dummy_data.dart';
 import 'package:meals_app/widgets/category_item.dart';
 
@@ -7,21 +7,25 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return Scaffold(
+      appBar: AppBar(title: const Text('DeliMeal')),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map(
+              (category) => CategoryItem(
+                key: Key(category.id),
+                category: category,
+              ),
+            )
+            .toList(),
       ),
-      children: DUMMY_CATEGORIES
-          .map(
-            (category) => CategoryItem(
-              key: Key(category.id),
-              category: category,
-            ),
-          )
-          .toList(),
     );
   }
 }
